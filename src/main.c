8be0ffafd27ce7e95367b8fafa4bfff63c65b1c4
eviiiiiian <94224468+Evian-Efrei@users.noreply.github.com>
2025-12-12 14:49:39 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -8,6 +8,11 @@ int main(int argc, char *argv[]){
 	{
 	char* op = argv[1];
 	char* a = argv[2];
+	double r = 0;
+	if (argc == 4)
+	{
+	char* op = argv[1];
+	char* a = argv[2];
 	char* b = argv[3];
 	double r = 0;
 	if (strcmp(op, "add") == 0) {
